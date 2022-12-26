4498cf76a5b5b4c494b469a11bdb1ebaaf9b8b5b
Azizkhan1992 <azizkhannasrullayev1992@gmail.com>
2022-12-26 18:51:39 +0500

    22 change

--- src/views/press/openDataSingle.vue
+++ src/views/press/openDataSingle.vue
@@ -263,7 +263,7 @@ export default {
       await this.$api.get(`/press/opendata/${this.getID}`)
       .then(resp => {
         this.chosenOpenData = resp.data.result
-        console.log(this.chosenOpenData)
+        // console.log(this.chosenOpenData)
       }).catch(err => {console.log(err)})
     },
     filPost(val) {
